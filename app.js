fe8d772873cf394f3387899ddaba42a0bb3427c0
Lee Morgan <Morgan.LeeR@protonmail.com>
2019-03-20 14:48:57 +0500

    Add bio.ejs.  Create main page menu

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(request, response){
     response.render("home.ejs");
+});
+
+app.get("/bio", function(request, response){
+    response.render("bio.ejs");
 });
 
 app.listen(8080);
